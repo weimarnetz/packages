082c8cd65229393f38b2374a7dcb2a4567a87d22
Andreas Bräu <ab@andi95.de>
2022-07-25 09:07:51 +0200

    typo

--- utils/weimarnetz-button-config/root/etc/uci-defaults/99-ffwizard-buttons.sh
+++ utils/weimarnetz-button-config/root/etc/uci-defaults/99-ffwizard-buttons.sh
@@ -7,6 +7,7 @@ uci_add ffwizard node "buttons"
 
 uci_commit ffwizard
 
-mv /etc/rc.button/reset.net /etc/rc.button/reset
+mv /etc/rc.button/reset /etc/rc.button/reset.orig
+mv /etc/rc.button/reset.new /etc/rc.button/reset
 
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
