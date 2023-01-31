e1e6f24b5984adb05fea6dfa10a60ceadb462754
Andreas Bräu <ab@andi95.de>
2023-01-31 23:06:32 +0100

    fastd: disable vtun on install
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- net/weimarnetz-fastd/root/etc/uci-defaults/99-fastd.sh
+++ net/weimarnetz-fastd/root/etc/uci-defaults/99-fastd.sh
@@ -10,4 +10,8 @@ uci_add_list ffwizard fastd url '"fastd.v.weimarnetz.de" port 10000'
 uci_set ffwizard fastd pubkey "09f8934b3e7e684be6dc8fd0cfeeea1bf30ab701aa83ae6f916f82c6993309fe"
 uci_commit ffwizard
 
+# disable vtun
+sed -i -e '/vpnwatchdog$/d' /etc/crontabs/root
+rm /etc/hotplug.d/iface/90-vpn
+
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
