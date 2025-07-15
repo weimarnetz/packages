c7e257cf11f9ff8470db8c592e1bca63288f3f12
Andreas Bräu <ab@andi95.de>
2025-07-15 20:58:29 +0200

    disable wps buttons globally

--- utils/weimarnetz-button-config/root/etc/uci-defaults/99-ffwizard-buttons.sh
+++ utils/weimarnetz-button-config/root/etc/uci-defaults/99-ffwizard-buttons.sh
@@ -8,5 +8,6 @@ uci_add ffwizard node "buttons"
 uci_commit ffwizard
 
 mv /etc/rc.button/reset.new /etc/rc.button/reset
+mv /etc/rc.button/wps.new /etc/rc.button/wps
 
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
